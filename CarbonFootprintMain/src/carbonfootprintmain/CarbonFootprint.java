93265329817450058daa3d4bf76dbccd08bad0e3
Samama Khan <37092889+sam67k@users.noreply.github.com>
2021-04-23 15:33:45 +0500

    Delete CarbonFootprintMain directory

--- CarbonFootprintMain/src/carbonfootprintmain/CarbonFootprint.java
+++
@@ -1,9 +0,0 @@
-
-package carbonfootprintmain;
-
-public interface CarbonFootprint
-{
-    
-    public void getCarbonFootprint();
-    
-}
--- CarbonFootprintMain/src/carbonfootprintmain/Vehicle.java
+++
@@ -1,7 +0,0 @@
-
-package carbonfootprintmain;
-
-public class Vehicle
-{
-    
-}
